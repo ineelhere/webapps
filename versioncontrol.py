7c32ca2078faa6a7750000db4db7f27c8e979943
Indraneel Chakraborty <64887729+ineelhere@users.noreply.github.com>
2021-06-22 11:58:39 +0530

    Update versioncontrol.py

--- versioncontrol.py
+++ versioncontrol.py
@@ -4,11 +4,11 @@ st.write("""
 # Version control in 2 code snippets 
 **For experimentation purpose only | Indraneel Chakraborty** 
     """)
-l1 = st.text_area("Enter the first list of data. One data per line")
-l2 = st.text_area("Enter the second list of data. One data per line")
+l1 = st.text_area("Enter the first code snippet")
+l2 = st.text_area("Enter the modified code snippet")
 l1.strip().splitlines()
 l2.strip().splitlines()
 l3 = list(set(l1+l2))
 st.markdown("## Tracing changes")
-for line in difflib.unified_diff(l1, l2):
+for line in difflib.unified_diff(l1, l2, fromfile='file1', tofile='file2', lineterm=''):
     st.write(line)
