2e73666f040a520683649681b983b60bf74a6e8c
Indraneel Chakraborty <64887729+ineelhere@users.noreply.github.com>
2021-06-30 13:02:27 +0530

    update

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pandas as pd
 import speedtest
-import datetime
+from datetime import datetime
+import pytz
 
 st.write("# Internet Connection Speed Test")  
 spd = speedtest.Speedtest()
@@ -14,9 +15,9 @@ ip = pd.DataFrame.from_dict(spd.get_config()['client'], orient='index')
 # ip = spd.get_config()['client']['ip']
 st.table(ip)
 
-
-x = datetime.datetime.now()
-st.write(f'Test finished on : {x.strftime("%d")} {x.strftime("%B")} {x.strftime("%Y")} at {x.strftime("%I")}:{x.strftime("%M")}:{x.strftime("%S")} {x.strftime("%p")} \n ### Have a great day!')
+IST = pytz.timezone('Asia/Kolkata') 
+datetime_ist = datetime.now(IST)
+st.write(f"Test finished on - {datetime_ist.strftime('%d/%m/%Y %H:%M:%S %Z')}")
 
 st.write(""" * github: https://github.com/ineelhere/webapps/blob/main/app.py 
 * docker: https://hub.docker.com/r/ineelhere/netspeed/ """)
