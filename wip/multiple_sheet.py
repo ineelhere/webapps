3ed39b947fb8aa811ee182afa1655330d6e5f5e4
Indraneel Chakraborty <64887729+ineelhere@users.noreply.github.com>
2021-07-25 22:38:38 +0530

    Update multiple_sheet.py

--- wip/multiple_sheet.py
+++ wip/multiple_sheet.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd 
 def multiple_sheet(df):
-    st.write("We noticed your file has multiple sheets.")
+    st.write("**We noticed your file has multiple sheets.**")
     sheet = st.selectbox(" Please select the sheet you would like to work on now.", df.sheet_names)
-    df = pd.read_excel(file_u, sheet)
+    df = pd.read_excel(df, sheet)
     return(df)
